ac55ad2da0acb654def1ef830349ced3d13b3b67
João Marcelo Oliveira de Souza <joaomarcelo9393@gmail.com>
2020-04-14 19:51:26 -0300

    Rename methods

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,11 +1,11 @@
 defmodule Identicon do
   def main(input) do
     input
-    |> hash()
-    |> pick_color()
+    |> set_hash()
+    |> set_color()
   end
 
-  defp hash(input) do
+  defp set_hash(input) do
     hash =
       :crypto.hash(:md5, input)
       |> :binary.bin_to_list()
@@ -13,7 +13,7 @@ defmodule Identicon do
     %Identicon.Image{hex: hash}
   end
 
-  defp pick_color(%Identicon.Image{hex: [r, g, b | _]} = image) do
+  defp set_color(%Identicon.Image{hex: [r, g, b | _]} = image) do
     %Identicon.Image{image, color: {r, g, b}}
   end
 end
