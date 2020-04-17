5f70a6dc6d05f6db378592d3d4cdc955fa06ffe9
João Marcelo Oliveira de Souza <joaomarcelo9393@gmail.com>
2020-04-16 21:42:43 -0300

    Filter odd squares

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,3 +1,5 @@
+require Integer
+
 defmodule Identicon do
   def main(input) do
     input
@@ -8,40 +10,34 @@ defmodule Identicon do
 
   defp set_hash(input) do
     %Identicon.Image{
-      hex:
+      hash:
         :crypto.hash(:md5, input)
         |> :binary.bin_to_list()
     }
   end
 
-  defp set_color(%Identicon.Image{hex: [r, g, b | _]} = image) do
+  defp set_color(%Identicon.Image{hash: [r, g, b | _]} = image) do
     %Identicon.Image{image | color: {r, g, b}}
   end
 
-  defp set_grid(%Identicon.Image{hex: hex} = image) do
+  defp set_grid(%Identicon.Image{hash: hash} = image) do
     %Identicon.Image{
       image
       | grid:
-          hex
-          |> hex_to_grid()
-          |> boolean_grid()
-          |> mirror_rows()
+          hash
+          |> Enum.chunk_every(3, 3, :discard)
+          |> Enum.map(&mirror_row/1)
+          |> List.flatten()
+          |> Enum.with_index()
+          |> Enum.filter(&is_even/1)
     }
   end
 
-  defp hex_to_grid(hex) do
-    Enum.chunk_every(hex, 3, 3, :discard)
+  defp mirror_row([a, b, c]) do
+    [a, b, c, b, a]
   end
 
-  defp boolean_grid(grid) do
-    Enum.map(grid, fn row -> row_to_boolean(row) end)
-  end
-
-  defp row_to_boolean(row) do
-    Enum.map(row, fn i -> Kernel.trunc(i) |> Kernel.rem(2) end)
-  end
-
-  defp mirror_rows(grid) do
-    Enum.map(grid, fn [a, b, c] -> [a, b, c, b, a] end)
+  defp is_even({code, _index}) do
+    rem(code, 2) == 0
   end
 end
--- lib/identicon/image.ex
+++ lib/identicon/image.ex
@@ -1,3 +1,3 @@
 defmodule Identicon.Image do
-  defstruct hex: nil, color: nil, grid: nil
+  defstruct hash: nil, color: nil, grid: nil
 end
